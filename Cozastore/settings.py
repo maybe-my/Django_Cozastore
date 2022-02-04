c2d54c1574a0aedaf8c0a763275d42e4929be786
load <load3585@protonmail.com>
2022-02-04 20:57:48 +0200

    add image to admin panel

--- Cozastore/settings.py
+++ Cozastore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egkbrlhulxps@8$_w9k^ao*m(wuq5mqjuq6kcke@z3fv6w9&uw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.7']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- ShopApp/admin.py
+++ ShopApp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Product, Category, News
+from import_export.admin import ImportExportActionModelAdmin
+from import_export import resources
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
 
 
 # @admin.register(Product)
@@ -23,17 +29,9 @@ class NewsletterAdmin(admin.ModelAdmin):
     list_filter = ['email']
 
 
-
-# Export/Import
-from import_export.admin import ImportExportActionModelAdmin
-from import_export import resources
-from import_export import fields
-from import_export.widgets import ForeignKeyWidget
-
-
-
 class ProductsResource(resources.ModelResource):
     category = fields.Field(column_name='category', attribute='category', widget=ForeignKeyWidget(Category, 'name'))
+
     class Meta:
         model = Product
 
@@ -41,8 +39,13 @@ class ProductsResource(resources.ModelResource):
 @admin.register(Product)
 class ProductsResource(ImportExportActionModelAdmin):
     resource_class = ProductsResource
-    list_display = ['name', 'price', 'available', 'created']
+    list_display = ['name', 'image_show', 'price', 'available', 'created']
     list_filter = ['available', 'created', 'updated']
     prepopulated_fields = {'slug': ('name',)}
 
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return "None"
 
+    image_show.__name__ = "Image"
