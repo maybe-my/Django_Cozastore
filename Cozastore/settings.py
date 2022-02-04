1d924670a25669b31c79779bf2dea2f61747b218
load <load3585@protonmail.com>
2022-02-04 18:14:52 +0200

    Add export/import tovars

--- Cozastore/settings.py
+++ Cozastore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egkbrlhulxps@8$_w9k^ao*m(wuq5mqjuq6kcke@z3fv6w9&uw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.6']
+ALLOWED_HOSTS = ['192.168.1.7']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ShopApp'
+    'import_export',
+    'ShopApp',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-Ru'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
--- ShopApp/admin.py
+++ ShopApp/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Product, Category, News
 
 
-@admin.register(Product)
-class ProductsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'available', 'created']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+# @admin.register(Product)
+# class ProductsAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'price', 'available', 'created']
+#     list_filter = ['available', 'created', 'updated']
+#     # list_editable = ['price', 'available']
+#     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Category)
@@ -21,5 +21,28 @@ class CategoryAdmin(admin.ModelAdmin):
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ['email']
     list_filter = ['email']
+
+
+
+# Export/Import
+from import_export.admin import ImportExportActionModelAdmin
+from import_export import resources
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
+
+
+
+class ProductsResource(resources.ModelResource):
+    category = fields.Field(column_name='category', attribute='category', widget=ForeignKeyWidget(Category, 'name'))
+    class Meta:
+        model = Product
+
+
+@admin.register(Product)
+class ProductsResource(ImportExportActionModelAdmin):
+    resource_class = ProductsResource
+    list_display = ['name', 'price', 'available', 'created']
+    list_filter = ['available', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
 
 
