0186b60269b1849158e5bbc33a44d65b5ae9fefc
load <load3585@protonmail.com>
2022-02-05 18:17:04 +0200

    fix cart. Add orders

--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('order/', include('OrdersApp.urls')),
     path('products/', views.ProductList.as_view(), name='products_list'),
     path('detail/<slug>', views.DetailProduct.as_view(), name='detail_product'),
     path('about/', views.About.as_view(), name='about'),
