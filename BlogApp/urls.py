f8bede31cf5108adf1c4389067697f01ba3cbe5c
load <load3585@protonmail.com>
2022-02-05 21:52:32 +0200

    fix blog

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
+
+app_name = 'BlogApp'
+
 urlpatterns = [
     path('', views.Index.as_view(), name='blog_index'),
     path('detail/<slug>', views.DetailPost.as_view(), name='blog_detail'),
